7117e9d15bcbb3099bacd3b920e11bff14ff0ca4
rainie <463103063@qq.com>
2017-02-09 09:58:44 +0800

    重构了工具库的打包流程 (#1)
    * 📦  [refact] 优化打包流程，压缩配置
    
    * chore(package.json): updaten version to 1.0.4 by cqGit

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+const babel = require('rollup-plugin-babel');
+const fs = require('fs');
+const path = require('path');
+const babelRc = JSON.parse(fs.readFileSync('.babelrc','utf8'));
+
+export default {
+    entry: 'src/index.js',
+    plugins: [
+        babel({
+            babelrc: true,
+            exclude: 'node_modules/**'
+        })
+    ],
+    format: 'umd',
+    sourceMap: false,
+};
